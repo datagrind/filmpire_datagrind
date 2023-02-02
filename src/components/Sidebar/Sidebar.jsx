617893f5712823f41611acfd7a6a4adc9f290403
unknown <https://github.com/datagrind/filmpire_datagrind.git>
2023-02-02 15:59:20 -0600

    genre and movie category types

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,14 +4,12 @@ import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import { ClassNames } from '@emotion/react';
+import { useDispatch, useSelector } from 'react-redux';
 import useStyles from './styles';
+import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
-const demoCategories = [
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-];
 const categories = [
   { label: 'Popular', value: 'popular' },
   { label: 'Top Rated', value: 'top_rated' },
@@ -22,8 +20,12 @@ const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2f
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 const Sidebar = ({ setMobileOpen }) => {
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
   const theme = useTheme();
   const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+  const dispatch = useDispatch();
+
   return (
     <>
       <Link to="/" className={classes.imageLink}>
@@ -38,10 +40,10 @@ const Sidebar = ({ setMobileOpen }) => {
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
-            <ListItemButton onClick={() => {}}>
-              {/* <ListItemIcon>
-                <img src={redLogo} className={classes.genreImages} height={30} />
-              </ListItemIcon> */}
+            <ListItemButton onClick={() => dispatch(selectGenreOrCategory(value))}>
+              <ListItemIcon>
+                <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
+              </ListItemIcon>
               <ListItemText primary={label} />
             </ListItemButton>
           </Link>
@@ -50,13 +52,17 @@ const Sidebar = ({ setMobileOpen }) => {
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
-            <ListItemButton onClick={() => {}}>
-              {/* <ListItemIcon>
-              <img src={redLogo} className={classes.genreImages} height={30} />
-            </ListItemIcon> */}
-              <ListItemText primary={label} />
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress size="4rem" />
+          </Box>
+        ) : data.genres.map(({ name, id }) => (
+          <Link key={id} className={classes.links} to="/">
+            <ListItemButton onClick={() => dispatch(selectGenreOrCategory(id))}>
+              <ListItemIcon>
+                <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
+              </ListItemIcon>
+              <ListItemText primary={name} />
             </ListItemButton>
           </Link>
         ))}
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 export { default as Actors } from './Actors/Actors';
 export { default as Movies } from './Movies/Movies';
 export { default as MovieInformation } from './MovieInformation/MovieInformation';
-// eslint-disable-next-line import/no-cycle
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profiles } from './Profiles/Profiles';
 export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as MovieList } from './MovieList/MovieList';
+export { default as Movie } from './Movie/Movie';
